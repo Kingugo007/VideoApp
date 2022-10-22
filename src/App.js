158d908d5b9e05d9d08ccdc3a2766b79cc3f5d34
Ugochukwu <oluougochukwu@gmail.com>
2022-10-22 21:30:33 +0100

    make it better

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
+import { Typography, AppBar } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles'
+
+
+import Videoplayer from './components/VideoPlayer';
+import Options from './components/Option';
+import Notifications from './components/Notifications';
+
+
+const useStyles = makeStyles((theme) => ({
+    appBar: {
+      borderRadius: 15,
+      margin: '30px 100px',
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: '600px',
+      border: '2px solid black',
+  
+      [theme.breakpoints.down('xs')]: {
+        width: '90%',
+      },
+    },
+    image: {
+      marginLeft: '15px',
+    },
+    wrapper: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      width: '100%',
+    },
+  }));
+
+const App = () => {
+  const classes = useStyles();
+
+
+
+    return (
+        <MuiThemeProvider>
+        <div className={classes.wrapper}>    
+         <AppBar className={classes.appBar} position='static' color='inherit'>
+             <Typography variant='h2' align="center">
+                UG VIDEO CHAT
+             </Typography>
+         </AppBar>
+         <Videoplayer />
+         <Options>
+             <Notifications />
+         </Options>
+        </div>
+        </MuiThemeProvider>
+    );
+}
+
+export default App;
